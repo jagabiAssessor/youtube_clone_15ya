d15947fd7d959768130c97a6e7f63614e997c3ff
OGUbanana <bbeunjin@gmail.com>
2023-07-25 15:07:06 +0900

    feet: Menu toggle sidebar
    사이드바 기능 구현, css 파일 따라 수정 필요

--- js/index.js
+++ js/index.js
@@ -3,20 +3,20 @@ const sidebar = document.querySelector('.sidebar'); // 사이드바
 
 // 클릭시 사이드바 토글
 menu.addEventListener('click', () => {
-  document.body.classList.toggle('show-sidebar');
+  document.body.classList.toggle('showSidebar');
 });
 
 // 화면 크기가 변경될 때 사이드바 상태 업데이트
 const mediaQuery = window.matchMedia('(min-width: 769px)');
 mediaQuery.addEventListener('change', (event) => {
   if (event.matches) {
-    document.body.classList.remove('show-sidebar');
+    document.body.classList.remove('showSidebar');
   }
 });
 
 // 사이드 바와 메뉴 버튼 외 영역 클릭시 사이드바 숨김
 document.addEventListener('click', (event) => {
   if (!sidebar.contains(event.target) && !menu.contains(event.target)) {
-    document.body.classList.remove('show-sidebar');
+    document.body.classList.remove('showSidebar');
   }
 });
