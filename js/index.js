f2e047a81b3ee36b184e92a40c8b36cb8ff94a20
jeipark <parkdda310@naver.com>
2023-07-26 17:31:42 +0900

    Update index Update style

--- js/index.js
+++ js/index.js
@@ -1,6 +1,38 @@
-const menu = document.querySelector('#menu'); // menu 버튼
 const sidebar = document.querySelector('.sidebar'); // 사이드바
+const menu = document.querySelector('#menu'); // menu 버튼
 
+// 비디오 페이지에 들어갈 유튜브 플레이어
+var player;
+    function onYouTubeIframeAPIReady() {
+      player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: '',                                  // Video ID 입력
+        events: {
+          'onReady': onPlayerReady
+        }
+      });
+    }
+
+    function onPlayerReady(event) {
+      event.target.playVideo();
+    }
+
+    function playVideo() {
+      player.playVideo();
+    }
+
+    function pauseVideo() {
+      player.pauseVideo();
+    }
+
+    function stopVideo() {
+      player.stopVideo();
+    }
+
+
+  
+//  -------------------- 은진님 하신부분 -----------------------------------------
 const updateSidebarState = () => {
   const isLargeScreen = window.matchMedia('(min-width: 769px)').matches; // 화면 크기 확인
 
@@ -36,3 +68,53 @@ document.addEventListener('click', (event) => {
     document.body.classList.remove('showSidebar');
   }
 });
+// -----------------------------------------------------------------------------
+
+
+
+// 메인으로 비디오 가져오기
+
+// 비디오 리스트                               홈페이지에 나와있는 비디오 갯수가 20개인데 
+for (var i=0; i<videos.length; i++){     //이부분을 반복문으로 만들어주시면 코드가 깔끔해질거 같습니다.
+  var videos = [
+  {title: 'Video 1', id: 'video1'},
+  {title: 'Video 2', id: 'video2'},
+  {title: 'Video 3', id: 'video3'},
+  {title: 'Video 4', id: 'video4'},
+  {title: 'Video 5', id: 'video5'},
+  {title: 'Video 6', id: 'video6'},
+  {title: 'Video 7', id: 'video7'},
+  {title: 'Video 8', id: 'video8'},
+  {title: 'Video 9', id: 'video9'},
+]};
+
+// 비디오 가져오기 
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+  if (xhr.readState === XMLHttpRequest.DONE){
+    if (xhr.status === 200){                      // 비디오가 잘 받아와 졌으면 실행
+      function loadVideos() {
+        var videoList = document.getElementById('videoList');
+        videoList.innerHTML = '';
+      
+        for(var i=0; i<videos.length; i++) {
+            var video = document.createElement('div');
+            video.innerHTML = '<h3>' + videos[i].title + '</h3><button onclick="playVideo(\'' + videos[i].id + '\')">Play</button>';
+            videoList.appendChild(video);
+        }
+      }
+    } else {                                     // 비디오 가져오기 실패 했을때 실행 
+        alert('새로고침 해주세요.')
+    }
+  };
+  xhr.open('GET','http://oreumi.appspot.com/video/getVideoList&t=')
+}
+
+// 비디오 플레이
+function playVideo(id) {
+  alert('Playing video: ' + id);
+}
+
+
+
+
