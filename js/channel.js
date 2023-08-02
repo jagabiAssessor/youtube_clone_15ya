fa6a126899cbeb7cabfb65429170ac4b3eb87974
bnz18 <benuze18@gmail.com>
2023-08-02 11:25:15 +0900

    feat: channel.js에 채널 배너 호출기능 구현

--- js/channel.js
+++ js/channel.js
@@ -1,4 +1,5 @@
 // JavaScript (channel.html)
+
 document.addEventListener("DOMContentLoaded", function () {
     // URL에서 채널 이름을 가져옵니다.
     const queryString = window.location.search;
@@ -58,9 +59,16 @@ function displayVideoThumbnail(videoInfo) {
     videoGrid.appendChild(videoContainer);
 }
 
+
 function displayChannelInfo(channelData) {
     // 채널 정보를 받아와서 HTML 구조를 동적으로 생성합니다.
     const channelWrapper = document.getElementById("channelInfoContainer");
+
+    // 채널 배너 받아와서 업데이트
+    const channelCover = document.querySelector('.Channel-Cover img');
+    if (channelCover && channelData.channel_banner) {
+        channelCover.src = channelData.channel_banner;
+    }
 
     // 채널 사진 이미지 엘리먼트를 생성합니다.
     const channelImage = document.createElement("img");
@@ -71,18 +79,24 @@ function displayChannelInfo(channelData) {
     const channelNameElement = document.createElement("div");
     channelNameElement.classList.add("channel-name");
     channelNameElement.innerText = channelData.channel_name; // API에서 받아온 채널명을 사용
-    
 
     // 구독자수를 표시할 엘리먼트를 생성합니다.
     const subscriberCountElement = document.createElement("div");
     subscriberCountElement.classList.add("subscriber-count");
-    subscriberCountElement.innerText = `구독자수: ${channelData.subscribers}명`; // API에서 받아온 구독자수를 사용
+    const subscriberCountElement_m = Math.round(channelData.subscribers / 10000);
+    subscriberCountElement.innerText = `구독자수: ${subscriberCountElement_m}명`; // API에서 받아온 구독자수를 사용
+
+    // 채널 정보를 담는 엘리먼트를 생성합니다.
+    const channelInfoElement = document.createElement("div");
+    channelInfoElement.classList.add("channel-info");
+    channelInfoElement.appendChild(channelNameElement);
+    channelInfoElement.appendChild(subscriberCountElement);
 
     // 생성한 엘리먼트들을 채널 정보 컨테이너에 추가합니다.
     channelWrapper.appendChild(channelImage);
-    channelWrapper.appendChild(channelNameElement);
-    channelWrapper.appendChild(subscriberCountElement);
+    channelWrapper.appendChild(channelInfoElement);
 }
+
 
 document.getElementById("btn").addEventListener("click", function() {
     const imgBtn = document.querySelector(".img_btn");
@@ -98,32 +112,3 @@ document.getElementById("btn").addEventListener("click", function() {
         imgBtn.src = defaultImage;
     }
   });
-
-
-// function displayVideoThumbnail(videoInfo) {
-//     // div 요소 생성해서 videoContainer에 추가
-//     const videoContainer = document.createElement('div');
-//     videoContainer.className = 'video-container';
-
-//     // 비디오 요소 만들기
-//     const videoElem = document.createElement('video');
-//     videoElem.controls = true;
-//     videoElem.width = 276;  // 사이즈 피그마 참고
-//     videoElem.height = 155;
-//     // source 요소 생성해서 sourceElem에 추가
-//     const sourceElem = document.createElement('source');
-//     sourceElem.src = videoInfo.video_link;
-//     sourceElem.type = 'video/mp4';  // 비디오가 mp4 형식이라고 가정
-    
-//     videoElem.appendChild(sourceElem);
-//     videoContainer.appendChild(videoElem);
-
-//     // p 요소 생성해서 videoTitle에 추가
-//     const videoTitle = document.createElement('p');
-//     videoTitle.className = 'video-title';
-//     videoTitle.textContent = videoInfo.video_title;
-//     videoContainer.appendChild(videoTitle);
-    
-//     // videoGrid>videoContainer>videoTitle>sourceElem>videoInfo 이렇게 생각하면 된다
-//     videoGrid.appendChild(videoContainer);
-// }
