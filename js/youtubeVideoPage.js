93069bbbdb583537a47c2b077713ea8a54beb225
Geonyeong <renmsts@naver.com>
2023-08-04 03:53:08 +0900

    Update js

--- js/youtubeVideoPage.js
+++ js/youtubeVideoPage.js
@@ -8,7 +8,7 @@ async function fetchVideoInfo(videoId, isMainVideo = false) {
 
     xhr.open('GET', url, true);
 
-    xhr.onload = async function() {
+    xhr.onload = async function () {
         if (xhr.status >= 200 && xhr.status < 400) {
             const videoInfo = JSON.parse(xhr.responseText);
 
@@ -25,14 +25,14 @@ async function fetchVideoInfo(videoId, isMainVideo = false) {
             console.error('ID에 대한 비디오 정보를 가져오지 못했습니다:', videoId);
         }
     };
-    xhr.onerror = function() {
+    xhr.onerror = function () {
         console.error('네트워크 오류가 발생했습니다');
     };
     xhr.send();
 }
 
 //쿼리값으로 받은 id값으로 영상 정보 불러오기
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const receiveId = new URLSearchParams(window.location.search);
     const videoId = receiveId.get("video_id");
     if (videoId) {
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 const videoGrid = document.querySelector('.Video-Grid');
 
-const videoIds = Array.from({length: 21}, (_, i) => i);
+const videoIds = Array.from({ length: 21 }, (_, i) => i);
 
 videoIds.forEach(videoId => {
     fetchVideoInfo(videoId);
@@ -55,17 +55,17 @@ async function fetchVideoPlayer(videoId) {
     const url = `http://oreumi.appspot.com/video/getVideoInfo?video_id=${videoId}`;
 
     xhr.open('GET', url, true);
-    
-    xhr.onload = async function() {
+
+    xhr.onload = async function () {
         if (xhr.status >= 200 && xhr.status < 400) {
             const videoInfo = JSON.parse(xhr.responseText);
-            
-        // fetchChannelInfo 함수를 호출하여 채널 프로필 이미지 URL 가져오기
-        const channelProfileUrl = await fetchChannelInfo(videoInfo.video_channel);
-        videoInfo.channel_profile = channelProfileUrl.channel_profile; // 프로필 이미지 URL 객체에 추가
-        videoInfo.subscribers = channelProfileUrl.subscribers; // 구독자 수 추가
 
-        displayVideoInfo(videoInfo);
+            // fetchChannelInfo 함수를 호출하여 채널 프로필 이미지 URL 가져오기
+            const channelProfileUrl = await fetchChannelInfo(videoInfo.video_channel);
+            videoInfo.channel_profile = channelProfileUrl.channel_profile; // 프로필 이미지 URL 객체에 추가
+            videoInfo.subscribers = channelProfileUrl.subscribers; // 구독자 수 추가
+
+            displayVideoInfo(videoInfo);
 
 
         } else {
@@ -73,7 +73,7 @@ async function fetchVideoPlayer(videoId) {
         }
     };
 
-    xhr.onerror = function() {
+    xhr.onerror = function () {
         console.error('네트워크 오류가 발생했습니다');
     };
 
@@ -85,20 +85,14 @@ async function fetchVideoPlayer(videoId) {
 // 댓글 추가 될때 배경화면 늘려주는 코드
 const mainContent = document.querySelector('.Primary');
 
-function increaseBackgroundHeight() {
-    // 각 댓글마다 높이를 50px씩 올린다고 가정
-    // 필요할때 마다 값 조정
-    const heightIncrease = 50; 
-    const currentHeight = parseInt(getComputedStyle(mainContent).height);
-    const newHeight = currentHeight + heightIncrease;
-
-    mainContent.style.height = `${newHeight}px`;
-}
-
-// 댓글, 사용자 아바타 추가하는 코드
 function addComment() {
     const commentInput = document.querySelector('.CommentInput');
     let commentText = commentInput.value.trim();
+    // 2000자 제한
+    if (commentText.length > 2000) {
+        alert('댓글은 2000자 이하여야 합니다.');
+        return;
+    }
 
     if (commentText) {
         // 댓글 컨테이너 생성
@@ -113,20 +107,23 @@ function addComment() {
         // 댓글 텍스트 생성
         const commentTextDiv = document.createElement('div');
         commentTextDiv.className = 'comment-text';
-        
-        
-        // 댓글을 100자 간격으로 줄 바꿈하여 작성
-        const lineBreakInterval = 100;
-        let commentTextWithLineBreaks = '';
-        while (commentText.length > lineBreakInterval) {
-            commentTextWithLineBreaks += commentText.substring(0, lineBreakInterval) + '\n';
-            commentText = commentText.substring(lineBreakInterval);
-        }
-        commentTextWithLineBreaks += commentText;
-        commentTextDiv.innerText = commentTextWithLineBreaks;
+        commentTextDiv.innerText = commentText;
 
-        // 댓글 컨테이너에 아바타 및 댓글 텍스트 추가
+        // 사용자 아이디 생성 (Change 'user123' to the actual user ID)
+        const userIDDiv = document.createElement('div');
+        userIDDiv.className = 'user-id';
+        userIDDiv.innerText = 'officail_oreumi_15ya';
+
+        // 댓글 작성 시간 생성
+        const randomDate = getRandomDate();
+        const commentTimeDiv = document.createElement('div');
+        commentTimeDiv.className = 'comment-time';
+        commentTimeDiv.innerText = timeSince(randomDate) + ' ago';
+
+        // 댓글 컨테이너에 아바타, 댓글 텍스트, 아이디, 댓글 작성 시간 추가
         commentDiv.appendChild(avatarImg);
+        commentDiv.appendChild(userIDDiv);
+        commentDiv.appendChild(commentTimeDiv);
         commentDiv.appendChild(commentTextDiv);
 
         // 전체 댓글 컨테이너를 댓글 목록에 추가
@@ -144,16 +141,41 @@ function addComment() {
 }
 
 
+function getRandomDate() {
+    const startDate = new Date('2023-08-01');
+    const currentDate = new Date();
+    const randomTime = startDate.getTime() + Math.random() * (currentDate.getTime() - startDate.getTime());
+    return new Date(randomTime);
+}
+
+function timeSince(date) {
+    const currentDate = new Date();
+    const seconds = Math.floor((currentDate - date) / 1000);
+    if (seconds < 60) {
+        return seconds + ' seconds';
+    } else if (seconds < 3600) {
+        const minutes = Math.floor(seconds / 60);
+        return minutes + (minutes === 1 ? ' minute' : ' minutes');
+    } else if (seconds < 86400) {
+        const hours = Math.floor(seconds / 3600);
+        return hours + (hours === 1 ? ' hour' : ' hours');
+    } else {
+        const days = Math.floor(seconds / 86400);
+        return days + (days === 1 ? ' day' : ' days');
+    }
+}
+
 // 댓글 추가할 떄, 우측 동영사 메뉴 밀리는 현상 방지
 function increaseCommentsContainerHeight(commentsContainer) {
     // 각 댓글마다 높이를 50px씩 올린다고 가정
     // 필요할 때 마다 값 조정
-    const heightIncrease = 44.91;
+    const heightIncrease = 20;
     const currentHeight = parseInt(getComputedStyle(commentsContainer).height);
     const newHeight = currentHeight - heightIncrease;
 
     commentsContainer.style.height = `${newHeight}px`;
 }
+
 
 
 
@@ -165,7 +187,7 @@ function displayVideoInfo(videoInfo) {
 
     // 비디오 요소 만들기
     const displayVideo = document.createElement('video');
-    displayVideo.className='Youtube-Player'
+    displayVideo.className = 'Youtube-Player'
     displayVideo.controls = true;
     displayVideo.width = 1280;
     displayVideo.height = 720;
@@ -173,17 +195,17 @@ function displayVideoInfo(videoInfo) {
     const sourceElem = document.createElement('source');
     sourceElem.src = videoInfo.video_link;
     sourceElem.type = 'video/mp4';  // 비디오가 mp4 형식이라고 가정
-    
+
 
     displayVideo.appendChild(sourceElem);
     VideoPlayer.appendChild(displayVideo);
-    const VideoDesc= document.querySelector('.Video-Desc');
+    const VideoDesc = document.querySelector('.Video-Desc');
     VideoDesc.parentElement.insertBefore(VideoPlayer, VideoDesc);
 
 
 
     const VideoDescTitle = document.createElement('div');
-    VideoDescTitle.className='Video-DescTitle';
+    VideoDescTitle.className = 'Video-DescTitle';
     const videoTitle = document.createElement("h1");
     videoTitle.textContent = videoInfo.video_title;
     VideoDescTitle.appendChild(videoTitle);
@@ -195,7 +217,7 @@ function displayVideoInfo(videoInfo) {
     const channelProfile = document.createElement("img");
     channelProfile.className = "Profile";
     channelProfile.src = videoInfo.channel_profile;
-    channelProfile.style.borderRadius ="50%";
+    channelProfile.style.borderRadius = "50%";
     channelProfile.style.marginRight = "10px";
     channelProfile.width = 36;
     channelProfile.height = 36;
@@ -210,7 +232,7 @@ function displayVideoInfo(videoInfo) {
     channel.appendChild(channelName);
     channel.appendChild(document.createElement("br")); // 줄바꿈
     channel.appendChild(subscribers);
-    
+
 
     const DescButtonsub = document.querySelector('.DescButton_sub');
     DescButtonsub.parentElement.insertBefore(channelProfile, DescButtonsub);
@@ -225,12 +247,12 @@ function displayVideoInfo(videoInfo) {
     uploadDate.className = "upload-date";
     uploadDate.textContent = getTimeDiff(videoInfo.upload_date);
     const videoDetail = document.createElement('p');
-    videoDetail.textContent=videoInfo.video_detail;
-    
+    videoDetail.textContent = videoInfo.video_detail;
+
     Description.appendChild(videoViews);
     Description.appendChild(uploadDate);
     Description.appendChild(videoDetail);
-    
+
 }
 
 function sendToVideoPage(videoInfo) {
