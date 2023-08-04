90a02908d647bdf933bc8962fa69ca25b18dd044
bnz18 <benuze18@gmail.com>
2023-08-04 17:41:12 +0900

    fix: 채널 페이지 상단 동영상 추가 및 메인 페이지 동영상 수정
    fix: 채널 페이지 상단 동영상 추가
    
    채널 페이지 상단 동영상 추가로 인한 Channel.css, channel.html 수정
    
    메인 페이지 동영상 순서 정렬을 위해
    Mainpage.js 수정

--- js/Mainpage.js
+++ js/Mainpage.js
@@ -3,25 +3,39 @@ const videoGrid = document.querySelector('.video-grid');
 // 0부터 20까지의 video_id
 const videoIds = Array.from({length: 21}, (_, i) => i);
 // video_id 값 하나씩 함수에 대입
-videoIds.forEach(videoId => {
-    fetchVideoInfo(videoId);
-});
+// videoIds.forEach(videoId => {
+//     fetchVideoInfo(videoId);
+// });
 
+// Promise.all를 활용하여 videoId로 정리된 값 한 번에 처리
+Promise.all(videoIds.map(videoId => fetchVideoInfo(videoId)))
+    .then(videos => {
+        //
+        videos.forEach(video => displayVideoThumbnail(video));
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+// Mainpage Promise 사용하여 정렬
 async function fetchVideoInfo(videoId) {
-    const xhr = new XMLHttpRequest();
-    const url = `https://oreumi.appspot.com/video/getVideoInfo?video_id=${videoId}`;
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        const url = `https://oreumi.appspot.com/video/getVideoInfo?video_id=${videoId}`;
 
-    xhr.open('GET', url, true);
-    
-    xhr.onload = async function() {
-        if (xhr.status >= 200 && xhr.status < 400) {
-            const videoInfo = JSON.parse(xhr.responseText);
-            
-        // fetchChannelInfo 함수를 호출하여 채널 프로필 이미지 URL 가져오기
-        const channelProfileUrl = await fetchChannelInfo(videoInfo.video_channel);
-        videoInfo.channel_profile = channelProfileUrl.channel_profile; // 프로필 이미지 URL 객체에 추가
-        videoInfo.subscribers = channelProfileUrl.subscribers;
-            displayVideoThumbnail(videoInfo);
+        xhr.open('GET', url, true);
+        
+        xhr.onload = async function() {
+            if (xhr.status >= 200 && xhr.status < 400) {
+                const videoInfo = JSON.parse(xhr.responseText);
+                
+            // fetchChannelInfo 함수를 호출하여 채널 프로필 이미지 URL 가져오기
+            const channelProfileUrl = await fetchChannelInfo(videoInfo.video_channel);
+            videoInfo.channel_profile = channelProfileUrl.channel_profile; // 프로필 이미지 URL 객체에 추가
+            videoInfo.subscribers = channelProfileUrl.subscribers;
+        
+            resolve(videoInfo);
+        
         } else {
             console.error('ID에 대한 비디오 정보를 가져오지 못했습니다:', videoId);
         }
@@ -32,6 +46,8 @@ async function fetchVideoInfo(videoId) {
     };
 
     xhr.send();
+    
+    });
 }
 
 
