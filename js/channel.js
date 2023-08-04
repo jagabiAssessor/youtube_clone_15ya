61f44beba8620d82e212ff71f17f80a40868519b
kys <crosstosaw@naver.com>
2023-08-04 13:32:05 +0900

    type: feat

--- js/channel.js
+++ js/channel.js
@@ -1,104 +1,89 @@
 // JavaScript (channel.html)
 
 document.addEventListener("DOMContentLoaded", function () {
-    // URL에서 채널 이름을 가져옵니다.
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const channelName = urlParams.get('channel');
 
-    // API 요청을 위한 URL을 생성합니다.
-    const apiUrl = `http://oreumi.appspot.com/channel/getChannelInfo?video_channel=${encodeURIComponent(channelName)}`;
-    const xhr = new XMLHttpRequest();
+    const channelInfoUrl = `https://oreumi.appspot.com/channel/getChannelInfo?video_channel=${encodeURIComponent(channelName)}`;
+    const channelXhr = new XMLHttpRequest();
 
-    // API로부터 응답을 받았을 때 처리할 이벤트 핸들러를 등록합니다.
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                const response = JSON.parse(xhr.responseText);
-                displayChannelInfo(response);
+    channelXhr.onreadystatechange = function () {
+        if (channelXhr.readyState === 4) {
+            if (channelXhr.status === 200) {
+                const channelResponse = JSON.parse(channelXhr.responseText);
+                displayChannelInfo(channelResponse);
+
+                const videoInfoUrl = `https://oreumi.appspot.com/channel/getChannelVideo?video_channel=${encodeURIComponent(channelName)}`;
+                const videoXhr = new XMLHttpRequest();
+
+                videoXhr.onreadystatechange = function () {
+                    if (videoXhr.readyState === 4) {
+                        if (videoXhr.status === 200) {
+                            const videoResponse = JSON.parse(videoXhr.responseText);
+                            const videoIds = sortVideosByViews(videoResponse);
+
+                            // 비디오 정보를 병렬로 가져오기 위해 Promise.all 사용
+                            Promise.all(videoIds.map(fetchVideoInfo))
+                                .then(videos => {
+                                    videos.forEach(displayVideoThumbnail);
+                                })
+                                .catch(error => {
+                                    console.error('비디오 정보를 가져오는 중에 에러가 발생했습니다:', error);
+                                });
+                        } else {
+                            console.error('비디오 정보를 가져오는 중에 에러가 발생했습니다.');
+                        }
+                    }
+                };
+
+                videoXhr.open('POST', videoInfoUrl);
+                videoXhr.send();
             } else {
-                console.error('Error fetching data. Status:', xhr.status);
+                console.error('채널 정보를 가져오는 중에 에러가 발생했습니다. 상태:', channelXhr.status);
             }
         }
     };
 
-    // API에 GET 요청을 보냅니다.
-    xhr.open('POST', apiUrl);
-    xhr.send();
+    channelXhr.open('POST', channelInfoUrl);
+    channelXhr.send();
 });
 
+// 채널 비디오 정보를 조회수 순으로 video_id를 저장
+function sortVideosByViews(videoResponse) {
+    const sortedVideos = videoResponse.sort((a, b) => b.views - a.views);
+    return sortedVideos.map(video => video.video_id);
+}                       
 
-const videoGridTop = document.querySelector('.video-grid-top');
-const videoGrid = document.querySelector('.video-grid');
-
-//0부터 20까지의 video_id
-
-const videoIds = Array.from({length: 20}, (_, i) => i);
-
-// Promise.all을 통해 모든 Promise를 대기시킨다.
-// 그다음 날짜별로 동영상 정렬한 뒤 화면에 표시한다.
-// async await로 비동기 처리한 값은 .sort 해도 결국 전송되는 건 랜덤이다.
-Promise.all(videoIds.map(videoId => fetchVideoInfo(videoId)))
-    .then(videos => {
-        
-        // 조회수 별로 동영상 정렬
-        const mostViewedVideo = videos.sort((a, b) => b.views - a.views)[0];
-
-        displayVideoThumbnailTop(mostViewedVideo);
-
-        //날짜별로 동영상 정렬
-        videos = videos.filter(video => video.video_id !== mostViewedVideo.video_id);
-        videos.sort((a, b) => new Date(b.upload_date) - new Date(a.upload_date));
-        // 20개의 동영상 중 상위 6개 동영상
-        const earliestVideos = videos.slice(0, 6);
-
-        // 화면에 정렬된 비디오 표시
-        earliestVideos.forEach(video => displayVideoThumbnail(video));
-
-        // views가 가장 높은 동영상 상단에 놓는 함수(미완성)
-        // organizeVideos(mostViewedVideo.video_id);
-
-        // 이제 가장 많이 본 비디오를 그리드 맨 위에 배치합니다
-        setTimeout(() => {
-            // 각 비디오 요소가 해당 비디오 ID와 함께 데이터 속성을 가진다고 가정합니다
-            const mostViewedElement = videoGrid.querySelector(`[data-video-id="${mostViewedVideo.video_id}"]`);
-            if (mostViewedElement) {
-                videoGrid.prepend(mostViewedElement);
-            }
-        }, 0); // setTimeout를 사용하여 DOM 업데이트 확인
-    })
-    .catch(error => console.error('An error occurred:', error));
-
+//비디오 id를 이용해 비디오 정보를 가져오고 display함수로 전달
 function fetchVideoInfo(videoId) {
-    return new Promise(async (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
-        const url = `http://oreumi.appspot.com/video/getVideoInfo?video_id=${videoId}`;
+        const url = `https://oreumi.appspot.com/video/getVideoInfo?video_id=${videoId}`;
 
         xhr.open('GET', url, true);
 
-        xhr.onload = async function() {
+        xhr.onload = function () {
             if (xhr.status >= 200 && xhr.status < 400) {
                 const videoInfo = JSON.parse(xhr.responseText);
 
                 // fetchChannelInfo 함수를 호출하여 채널 프로필 이미지 URL 가져오기
-                try {
-                    const channelProfileUrl = await fetchChannelInfo(videoInfo.video_channel);
-                    videoInfo.channel_profile = channelProfileUrl.channel_profile;
-                    videoInfo.subscribers = channelProfileUrl.subscribers;
-
-                    // videoInfo로 promise resolve
-                    resolve(videoInfo);
-                } catch (error) {
-                    reject(new Error('Failed to fetch channel info'));
-                }
-
+                fetchChannelInfo(videoInfo.video_channel)
+                    .then(channelProfileUrl => {
+                        videoInfo.channel_profile = channelProfileUrl.channel_profile;
+                        videoInfo.subscribers = channelProfileUrl.subscribers;
+                        resolve(videoInfo);
+                    })
+                    .catch(error => {
+                        reject(error);
+                    });
             } else {
-                reject(new Error(`Failed to get video information for ID: ${videoId}`));
+                reject('ID에 대한 비디오 정보를 가져오지 못했습니다:', videoId);
             }
         };
 
-        xhr.onerror = function() {
-            reject(new Error('A network error occurred'));
+        xhr.onerror = function () {
+            reject('네트워크 오류가 발생했습니다');
         };
 
         xhr.send();
@@ -143,7 +128,17 @@ function displayChannelInfo(channelData) {
     channelWrapper.appendChild(channelInfoElement);
 }
 
+function sendToVideoPage(videoInfo) {
+    // video_id 보내기
+    window.location.href = `Video.html?video_id=${videoInfo.video_id}`;
 
+}
+
+function sendToChannelPage(videoInfo) {
+    window.location.href = `channel.html?channel=${videoInfo.video_channel}`;
+}
+
+const videoGrid = document.querySelector('.video-grid');
 document.getElementById("btn").addEventListener("click", function() {
     const imgBtn = document.querySelector(".img_btn");
   
@@ -158,144 +153,3 @@ document.getElementById("btn").addEventListener("click", function() {
         imgBtn.src = defaultImage;
     }
 });
-
-
-
-function sendToVideoPage(videoInfo) {
-    // video_id 보내기
-    window.location.href = `Video.html?video_id=${videoInfo.video_id}`;
-
-}
-
-function sendToChannelPage(videoInfo) {
-    window.location.href = `channel.html?channel=${videoInfo.video_channel}`;
-}
-
-
-
-
-
-// displayVideo.js의 displayVideoThumbnail과 같은 구조
-
-function displayVideoThumbnailTop(videoInfo) {
-    // div 요소 생성해서 videoContainer에 추가
-    const videoContainer = document.createElement('div');
-    videoContainer.className = 'video-container';
-
-
-    // img 요소 추가해서 썸네일 이미지 표시
-    const imageElem = document.createElement('img');
-    imageElem.className = "thumbnail"
-    imageElem.src = videoInfo.image_link;
-    imageElem.alt = "Thumbnail for " + videoInfo.video_title;
-    // imageElem.width = 276;
-    // imageElem.height = 155;
-    videoContainer.appendChild(imageElem);
-    // 비디오 요소 만들기
-    const videoElem = document.createElement('video');
-    videoElem.controls = true;
-    // videoElem.width = 276;  // 사이즈 피그마 참고
-    // videoElem.height = 155;
-    videoElem.style.display = 'none';  // 초기에는 비디오 숨김
-    // source 요소 생성해서 sourceElem에 추가
-    const sourceElem = document.createElement('source');
-    sourceElem.src = videoInfo.video_link;
-    sourceElem.type = 'video/mp4';  // 비디오가 mp4 형식이라고 가정
-    
-    videoElem.appendChild(sourceElem);
-    videoContainer.appendChild(videoElem);
-    
-
-
-    //채널
-    const channelProfile = document.createElement("img");
-    channelProfile.className = "channel-profile";
-    channelProfile.src = videoInfo.channel_profile;
-    channelProfile.width = 36;
-    channelProfile.height = 36;
-
-
-    const videoTitle = document.createElement("p");
-    videoTitle.className = "video-title";
-    videoTitle.textContent = videoInfo.video_title;
-
-
-    const videoChannel = document.createElement("p");
-    videoChannel.className = "video-channel";
-    videoChannel.textContent = videoInfo.video_channel;
-
-    const videoInfoContainer = document.createElement("div");
-    videoInfoContainer.className = "video-info-container";
-    videoInfoContainer.appendChild(channelProfile);
-    const videoInfomation = document.createElement("div");
-    videoInfomation.className = "video-info";
-    videoInfomation.appendChild(videoTitle);
-    videoInfomation.appendChild(videoChannel);
-
-    const videoViews = document.createElement("p");
-    videoViews.className = "video-views";
-    videoViews.textContent = "조회수 " + Math.round(videoInfo.views / 10000) + "만회 •";
-  
-    const uploadDate = document.createElement("p");
-    uploadDate.className = "upload-date";
-    uploadDate.textContent = getTimeDiff(videoInfo.upload_date);
-
-    const viewsUploadDateContainer = document.createElement("div");
-    viewsUploadDateContainer.className = "view-date";
-    viewsUploadDateContainer.appendChild(videoViews);
-    viewsUploadDateContainer.appendChild(uploadDate);
-    videoInfomation.appendChild(viewsUploadDateContainer);
-    videoInfoContainer.appendChild(videoInfomation);
-    videoContainer.appendChild(videoInfoContainer);
-
-
-    const videoClick = () => {
-        sendToVideoPage(videoInfo);
-      };
-      videoTitle.addEventListener('click', videoClick);
-      imageElem.addEventListener('click', videoClick);
-      videoElem.addEventListener('click', videoClick);
-      viewsUploadDateContainer.addEventListener('click', videoClick);
-
-    
-    const channelClick = () => {
-        sendToChannelPage(videoInfo);
-    };
-    channelProfile.addEventListener('click', channelClick);
-    
-
-    // 마우스 호버 기능
-    videoContainer.addEventListener('mouseover', () => {
-        imageElem.style.display = 'none';
-        videoElem.style.display = 'block';
-        videoElem.play();
-    });
-
-    videoContainer.addEventListener('mouseout', () => {
-        imageElem.style.display = 'block';
-        videoElem.style.display = 'none';
-        videoElem.pause();
-        videoElem.currentTime = 0;
-    });
-    // videoGrid>videoContainer>videoTitle>sourceElem>videoInfo 이렇게 생각하면 된다
-    videoGridTop.appendChild(videoContainer);
-}
-
-
-//날짜 계산
-function getTimeDiff(uploadDate) {
-    const currentDate = new Date();
-    const uploadedDate = new Date(uploadDate);
-    const diffTime = currentDate - uploadedDate;
-    const diffHours = diffTime / (1000 * 60 * 60);
-    const diffDays = diffHours / 24;
-    const diffMonths = diffDays / 30;
-    
-    if (diffMonths >= 1) {
-        return `${Math.floor(diffMonths)}개월 전`;
-    } else if (diffDays >= 1) {
-        return `${Math.floor(diffDays)}일 전`;
-    } else {
-        return `${Math.floor(diffHours)}시간 전`;
-    }
-}
